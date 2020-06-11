a6fcac81ea30588b16ef0da193fdb2aad35d8171
Abdul Salam <mysalamgh@gmail.com>
2020-06-11 20:43:57 +0800

    Created classes to hold the picture data.

---
+++ app/src/main/java/com/abdul/techm_abdul/models/Picture.kt
@@ -0,0 +1,11 @@
+package com.abdul.techm_abdul.models
+
+
+class Picture(
+    var albumId: Int,
+    var id: Int,
+    var title: String,
+    var url: String,
+    var thumbnailUrl: String
+) {
+}
