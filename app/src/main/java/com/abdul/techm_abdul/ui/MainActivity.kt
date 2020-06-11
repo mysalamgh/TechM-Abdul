b53badb29fe08a12877b381ba53e4c063a53f015
Abdul Salam <mysalamgh@gmail.com>
2020-06-11 20:33:56 +0800

    Created the main activity to manage the three fragments I intend to show the app screens

---
+++ app/src/main/java/com/abdul/techm_abdul/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.abdul.techm_abdul.ui
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.abdul.techm_abdul.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+}
